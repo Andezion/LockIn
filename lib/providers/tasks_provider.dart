0ce86222234c190b7398c7b1ef1472cb94ba5d78
Andezion <245122@edu.p.lodz.pl>
2026-01-06 15:59:21 +0100

    added new cool stuff

--- lib/providers/tasks_provider.dart
+++ lib/providers/tasks_provider.dart
@@ -2,12 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lockin/models/task.dart';
 import 'package:lockin/services/hive_service.dart';
 
-/// Provider for all tasks
 final tasksProvider = StateNotifierProvider<TasksNotifier, List<Task>>((ref) {
   return TasksNotifier();
 });
 
-/// Provider for tasks on a specific date
 final tasksForDateProvider = Provider.family<List<Task>, DateTime>((ref, date) {
   final allTasks = ref.watch(tasksProvider);
   return allTasks
@@ -24,25 +22,21 @@ class TasksNotifier extends StateNotifier<List<Task>> {
     state = HiveService.getAllActiveTasks();
   }
 
-  /// Add a new task
   Future<void> addTask(Task task) async {
     await HiveService.saveTask(task);
     _loadTasks();
   }
 
-  /// Update an existing task
   Future<void> updateTask(Task task) async {
     await HiveService.saveTask(task);
     _loadTasks();
   }
 
-  /// Delete a task (mark as inactive)
   Future<void> deleteTask(String taskId) async {
     await HiveService.deleteTask(taskId);
     _loadTasks();
   }
 
-  /// Reload tasks from database
   void reload() {
     _loadTasks();
   }
