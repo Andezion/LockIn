30900a2661161734c86b00892fa8877b5ed5704c
Andezion <245122@edu.p.lodz.pl>
2026-01-06 15:57:48 +0100

    cleaning up

--- lib/providers/day_entries_provider.dart
+++ lib/providers/day_entries_provider.dart
@@ -2,12 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lockin/models/day_entry.dart';
 import 'package:lockin/services/hive_service.dart';
 
-/// Provider for day entry on a specific date
 final dayEntryProvider = Provider.family<DayEntry?, DateTime>((ref, date) {
   return HiveService.getDayEntry(date);
 });
 
-/// Provider for updating day entries
 final dayEntriesNotifier = Provider<DayEntriesNotifier>((ref) {
   return DayEntriesNotifier(ref);
 });
@@ -17,13 +15,11 @@ class DayEntriesNotifier {
 
   DayEntriesNotifier(this.ref);
 
-  /// Update journal text for a specific date
   Future<void> updateJournal(DateTime date, String text) async {
     await HiveService.updateJournal(date, text);
     ref.invalidate(dayEntryProvider(date));
   }
 
-  /// Get day entry for a date
   DayEntry? getDayEntry(DateTime date) {
     return HiveService.getDayEntry(date);
   }
