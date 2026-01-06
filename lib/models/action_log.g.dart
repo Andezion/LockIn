f3e262ac6c4517cb382f7ccd094d2916b91f943c
Andezion <245122@edu.p.lodz.pl>
2026-01-06 15:53:56 +0100

    cleaning up

--- lib/models/action_log.g.dart
+++ lib/models/action_log.g.dart
@@ -1,10 +1,4 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
 part of 'action_log.dart';
-
-// **************************************************************************
-// TypeAdapterGenerator
-// **************************************************************************
 
 class ActionLogAdapter extends TypeAdapter<ActionLog> {
   @override
