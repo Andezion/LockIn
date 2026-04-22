84dc66e60fcfc1680e670741401696eab0e1a4f4
Andezion <245122@edu.p.lodz.pl>
2026-04-22 14:54:35 +0200

    add: added tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:LockIn/main.dart';
+import 'package:lockin/main.dart';
 
 void main() {
   testWidgets('App launches successfully', (WidgetTester tester) async {
